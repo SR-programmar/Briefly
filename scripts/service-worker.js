9be97e0fc4d5319dbfbb1554a1331112a32e85e8
Surya R <suryarajendran065@gmail.com>
2025-11-11 22:21:28 -0800

    Added proper text to speech with speechsynthesis.

--- scripts/service-worker.js
+++ scripts/service-worker.js
@@ -1,11 +1,14 @@
 
-
+// Listents to keyboard commands
 chrome.commands.onCommand.addListener(async (command) => {
 
+    // When user presses Ctrl+B
     if (command === 'summarize-page') {
 
-        let summarizedContent;
+        // Summarized content created from AI
+        let summarizedContent = "Hello there!";
 
+        // Fetches summarization from server
         async function summarizeContent() {
             const response = await fetch('https://summarizer-api-seven.vercel.app/', {
                 method: 'POST',
@@ -20,35 +23,29 @@ chrome.commands.onCommand.addListener(async (command) => {
             summarizedContent = data.summary;
         }
 
+       
         let tab = await getCurrentTab();
         
-        
-        chrome.scripting.executeScript({
-            target: { tabId: tab.id },
-            func: summarizeContent
-        })
-        
+        // Executes text-to-speech function
         chrome.scripting.executeScript({
             target: {tabId: tab.id},
             func: textToSpeech,
             args: [summarizedContent]
         })
-    
-
-        
-        
+     
     }
 })
 
 
-
+// Gets the current tab
 async function getCurrentTab() {
     const queryOptions = { active: true, currentWindow: true};
     const tabs = await chrome.tabs.query(queryOptions);
     return tabs[0];
 }
 
-
+// Converts given text to speech
 function textToSpeech(givenText) {
-    chrome.tts.speak(givenText);
+    const utterance = new SpeechSynthesisUtterance(givenText);
+    window.speechSynthesis.speak(utterance);
 }
